e6cf1a522394b6d6f111ca523e966e6b7614d25b
Desz01ate <deszolate@icloud.com>
2024-03-23 17:30:09 +0700

    Reverse the code publishing order

--- RooCodeAnnouncer/Implementations/HttpCodeReader.cs
+++ RooCodeAnnouncer/Implementations/HttpCodeReader.cs
@@ -30,7 +30,7 @@ public partial class HttpCodeReader : ICodeReader
         // skip header row
         var rows = htmlDoc.DocumentNode.SelectNodes("//tr").Skip(1);
 
-        foreach (var row in rows)
+        foreach (var row in rows.Reverse())
         {
             var children = row.ChildNodes.ToImmutableArray();
 
